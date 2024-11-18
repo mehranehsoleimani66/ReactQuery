d035a6ab4b50fe0225d8d97d336c27ca701d6ec3
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2024-11-18 09:43:44 +0330

    defined Type for fetching data in homePage

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,10 +1,25 @@
 import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+interface ProductState {
+  id: number;
+  name: string;
+  description: string;
+  year_of_creation: number;
+  rating: number;
+  views: number;
+  country: string;
+  price: number;
+}
 
 const Page = () => {
   const { data, isError, isLoading } = useQuery({
     queryKey: ['product'],
-    queryFn: () => {
-      return true;
+    queryFn: async () => {
+      const url = `http://localhost:5007/product`;
+      const response = await axios.get<ProductState>(url);
+      console.log(response.data);
+      return response.data;
     },
   });
   return (
